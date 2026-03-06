3639f9943ca6dd0f36843ce5476cdf51abfc5966
kuyf <mmhimer0@gmail.com>
2026-03-06 12:26:45 +0000

    first commit

---
+++ src/main/java/cires/bemodule/BeModuleApplication.java
@@ -0,0 +1,13 @@
+package cires.bemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cires/bemodule/BeModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package cires.bemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
