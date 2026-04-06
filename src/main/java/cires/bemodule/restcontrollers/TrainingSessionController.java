31da16a6248d573e247c2f249e3be1cbeed62053
kuyf <mmhimer0@gmail.com>
2026-04-06 16:51:42 +0100

    started TrainningSession impl

--- src/main/java/cires/bemodule/restcontrollers/TrainingSessionController.java
+++ src/main/java/cires/bemodule/restcontrollers/TrainingSessionController.java
@@ -1,19 +1,33 @@
 package cires.bemodule.restcontrollers;
 
+import cires.bemodule.dtos.TrainingSessionDTO;
+import cires.bemodule.entities.TrainingSession;
+import cires.bemodule.services.TrainingSessionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/sessions")
-public class SessionController {
+public class TrainingSessionController {
+
+    private final TrainingSessionService TrainingSessionService;
+
+    public TrainingSessionController(TrainingSessionService TrainingSessionService) {this.TrainingSessionService = TrainingSessionService;}
 
     @GetMapping
     public void getAllSessions() {}
 
     @PostMapping
-    public void createSession(@RequestBody Object session) {}
+    public ResponseEntity<TrainingSession> createSession(@RequestBody TrainingSession session) {
+        TrainingSession trainingSession = TrainingSessionService.createTrainingSession(session);
+        return ResponseEntity.ok(trainingSession);
+    }
 
     @GetMapping("/{id}")
-    public void getSessionById(@PathVariable Long id) {}
+    public ResponseEntity<TrainingSessionDTO> getSessionById(@PathVariable Long id) {
+        TrainingSessionDTO session = TrainingSessionService.getTrainingSessionById(id);
+        return ResponseEntity.ok(session);
+    }
 
     @PatchMapping("/{id}/status")
     public void updateSessionStatus(@PathVariable Long id, @RequestBody Object statusUpdate) {}
--- src/main/java/cires/bemodule/services/TrainingSessionService.java
+++ src/main/java/cires/bemodule/services/TrainingSessionService.java
@@ -1,23 +1,26 @@
 package cires.bemodule.services;
 
+import cires.bemodule.dtos.TrainingSessionDTO;
 import cires.bemodule.entities.TrainingSession;
+import cires.bemodule.mappers.TrainingSessionMapper;
 import cires.bemodule.repositories.TrainingSessionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service @RequiredArgsConstructor
 public class TrainingSessionService {
 
     private final TrainingSessionRepository trainingSessionRepository;
-    private final TrainerService trainerService;
-    private final ParticipantService participantService;
+//    private final TrainerService trainerService;
+//    private final ParticipantService participantService;
+    private final TrainingSessionMapper trainingSessionMapper;
 
-    public TrainingSessionService(TrainingSessionRepository trainingSessionRepository, TrainerService trainerService, ParticipantService participantService) {
-        this.trainingSessionRepository = trainingSessionRepository;
-        this.trainerService = trainerService;
-        this.participantService = participantService;
-
-    }
     public TrainingSession createTrainingSession(TrainingSession trainingSession) {
         return trainingSessionRepository.save(trainingSession);
+    }
+
+    public TrainingSessionDTO getTrainingSessionById(Long id) {
+        TrainingSession trainingSession = trainingSessionRepository.findById(id).orElseThrow( () -> new RuntimeException("Training session not found with id " + id));
+        return trainingSessionMapper.toDto(trainingSession);
     }
 }
