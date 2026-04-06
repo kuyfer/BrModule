4ac91a901dcfbde7110053888a06eea5bf984c25
kuyf <mmhimer0@gmail.com>
2026-04-06 13:55:06 +0100

    changed verifyKey due to getSignedKey being depricated

--- src/main/java/cires/bemodule/security/JwtAuthenticationFilter.java
+++ src/main/java/cires/bemodule/security/JwtAuthenticationFilter.java
@@ -33,47 +33,37 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String jwt = parseJwt(request);
-        String username = jwtService.extractUsername(jwt);
 
-        try {
-            if (jwt != null && jwtService.isTokenValid(jwt, userDetailsService.loadUserByUsername(username))) {
-
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
-                        userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (jwt != null) {
+            try {
+                String username = jwtService.extractUsername(jwt);
+                
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    
+                    if (jwtService.isTokenValid(jwt, userDetails)) {
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                                userDetails, 
+                                null,
+                                userDetails.getAuthorities()
+                        );
+                        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
+            } catch (Exception e) {
+                logger.error("Cannot set user authentication: {}");
             }
-        } catch (Exception e) {
-            logger.error("Cannot set user authentication: {}", e);
         }
 
         filterChain.doFilter(request, response);
     }
-//
-//        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-//
-//            if (jwtService.isTokenValid(jwt, userDetails)) {
-//                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-//                        userDetails,
-//                        null,
-//                        userDetails.getAuthorities()
-//                );
-//                authToken.setDetails(
-//                        new WebAuthenticationDetailsSource().buildDetails(request)
-//                );
-//                SecurityContextHolder.getContext().setAuthentication(authToken);
-//            }
-//        }
-//        filterChain.doFilter(request, response);
-//    }
+
     private String parseJwt(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
 
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
-            return headerAuth.substring(7, headerAuth.length());
+            return headerAuth.substring(7);
         }
 
         return null;
--- src/main/java/cires/bemodule/security/jwt/JwtService.java
+++ src/main/java/cires/bemodule/security/jwt/JwtService.java
@@ -66,13 +66,6 @@ public class JwtService {
         return claimsResolver.apply(extractAllClaims(token));
     }
 
-    public String generateToken(UserDetails userDetails) {
-        return buildToken(new HashMap<>(), userDetails, jwtExpiration);
-    }
-
-    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return buildToken(extraClaims, userDetails, jwtExpiration);
-    }
     public String generateToken(UserPrincipal userPrincipal) {
         Map<String, Object> claims = new HashMap<>();
 
@@ -88,11 +81,6 @@ public class JwtService {
         claims.put("permissions", permissions);
 
         return buildToken(claims, userPrincipal, jwtExpiration);
-    }
-
-
-    public String generateRefreshToken(UserDetails userDetails) {
-        return buildToken(new HashMap<>(), userDetails, refreshExpiration);
     }
 
     public String generateRefreshToken(UserPrincipal userPrincipal) {
@@ -127,7 +115,7 @@ public class JwtService {
     }
     public boolean validateJwtToken(String authToken) {
         try {
-            Jwts.parser().setSigningKey(SecretKey()).build().parse(authToken);
+            Jwts.parser().verifyWith(getSignInKey()).build().parse(authToken);
             return true;
         } catch (MalformedJwtException e) {
             logger.error("Invalid JWT token: {}", e.getMessage());
