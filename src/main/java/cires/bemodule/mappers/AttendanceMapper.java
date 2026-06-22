079139cd64191a952e7a831173fb3aeed4331477
kuyf <mmhimer0@gmail.com>
2026-06-22 18:47:01 +0100

    cleaned attendance mapper

--- src/main/java/cires/bemodule/mappers/AttendanceMapper.java
+++ src/main/java/cires/bemodule/mappers/AttendanceMapper.java
@@ -1,7 +1,6 @@
 package cires.bemodule.mappers;
 
 import cires.bemodule.dtos.responses.AttendanceResponse;
-import cires.bemodule.dtos.views.AttendanceDTO;
 import cires.bemodule.entities.Attendance;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,14 +8,8 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface AttendanceMapper {
 
-    AttendanceDTO toAttemdanceDto(Attendance attendance);
-
-    Attendance toAttendance(AttendanceDTO attendanceDTO);
-
     @Mapping(source = "session.id",           target = "sessionId")
     @Mapping(source = "session.title",        target = "sessionTitle")
     @Mapping(source = "participant.id",       target = "participantId")
-    AttendanceResponse toAttendanceResponse(Attendance attendance);
-
     AttendanceResponse toResponse(Attendance saved);
 }
