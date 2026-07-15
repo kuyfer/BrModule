411f52f291b1d829b3235ea7e8d01930827dc34a
kuyf <mmhimer0@gmail.com>
2026-07-15 17:31:31 +0100

    added dtos for create user new flow

--- src/main/java/cires/bemodule/dtos/requests/CreateUserRequest.java
+++ src/main/java/cires/bemodule/dtos/requests/CreateUserRequest.java
@@ -1,4 +1,36 @@
 package cires.bemodule.dtos.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class CreateUserRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Size(max = 100, message = "Email must not exceed 100 characters")
+    private String email;
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 50, message = "First name must not exceed 50 characters")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 50, message = "Last name must not exceed 50 characters")
+    private String lastName;
+
+    @NotEmpty(message = "At least one role is required")
+    private Set<Long> roleIds;
 }
--- src/main/java/cires/bemodule/dtos/requests/SetPasswordRequest.java
+++ src/main/java/cires/bemodule/dtos/requests/SetPasswordRequest.java
@@ -1,4 +1,22 @@
 package cires.bemodule.dtos.requests;
 
-public class SetPasswordRequest {
+import cires.bemodule.annotations.PasswordMatches;
+import cires.bemodule.interfaces.PasswordConfirmable;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@PasswordMatches
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class SetPasswordRequest implements PasswordConfirmable {
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Confirm password is required")
+    private String passwordConfirm;
 }
