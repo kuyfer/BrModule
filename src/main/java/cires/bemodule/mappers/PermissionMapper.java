822bc1feb1d08cd20e75626ba3c50b1c3401a9f5
kuyf <mmhimer0@gmail.com>
2026-07-15 17:44:03 +0100

    added a permission mapper

--- src/main/java/cires/bemodule/mappers/PermissionMapper.java
+++ src/main/java/cires/bemodule/mappers/PermissionMapper.java
@@ -1,4 +1,11 @@
 package cires.bemodule.mappers;
 
+import cires.bemodule.dtos.views.PermissionDTO;
+import cires.bemodule.entities.Permission;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface PermissionMapper {
+
+    PermissionDTO toPermissionDto(Permission permission);
 }
