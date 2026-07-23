759d8bea547cf3263d972903d00e6032eac9e963
kuyf <mmhimer0@gmail.com>
2026-07-23 17:58:20 +0100

    fixed subsidiaries count

--- src/main/java/cires/bemodule/mappers/OrganizationMapper.java
+++ src/main/java/cires/bemodule/mappers/OrganizationMapper.java
@@ -3,33 +3,20 @@ package cires.bemodule.mappers;
 import cires.bemodule.dtos.requests.CreateOrganizationRequest;
 import cires.bemodule.dtos.requests.PatchOrganizationRequest;
 import cires.bemodule.dtos.views.OrganizationDTO;
-import cires.bemodule.dtos.views.SubsidiarySummaryDTO;
 import cires.bemodule.entities.Organization;
-import cires.bemodule.entities.Subsidiary;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.Named;
 
-import java.util.List;
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { SubsidiaryMapper.class })
 public interface OrganizationMapper {
 
-//    @Mapping(target = "subsidiaryCount", expression = "java(organization.getSubsidiaries() != null ? organization.getSubsidiaries().size() : 0)")
-//    @Mapping(target = "subsidiaries", source = "subsidiaries", qualifiedByName = "toSummary")
+    @Mapping(target = "subsidiaryCount",
+            expression = "java(organization.getSubsidiaries() != null ? organization.getSubsidiaries().size() : 0)")
+    @Mapping(target = "subsidiaries", source = "subsidiaries")
     OrganizationDTO toDto(Organization organization);
 
     Organization toEntity(CreateOrganizationRequest request);
 
     void updateEntity(PatchOrganizationRequest request, @MappingTarget Organization entity);
-
-    @Named("toSummary")
-    default List<SubsidiarySummaryDTO> toSummary(List<Subsidiary> subsidiaries) {
-        if (subsidiaries == null) return List.of();
-        return subsidiaries.stream()
-                .map(s -> SubsidiarySummaryDTO.builder()
-                        .id(s.getId())
-                        .name(s.getName())
-                        .build())
-                        .toList();
-    }
 }
